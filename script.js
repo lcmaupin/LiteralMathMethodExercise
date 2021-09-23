159aa7128923f0003d3d6b122c195da712ab5af7
Luke Maupin <lukecmaupin@gmail.com>
2021-09-23 11:24:35 -0400

    Amends answers

--- script.js
+++ script.js
@@ -48,5 +48,6 @@ console.log(reindeers);
 console.log(Math.sqrt(2));
 
 // 9
-var newRandomNumber = Math.floor(Math.random() * 16 + 7);
+var newRandomNumber = Math.floor(Math.random() * 17 + 7);
+// var newRandomNumber = Math.floor(Math.random() * (23 - 7 + 1))
 console.log(newRandomNumber);
